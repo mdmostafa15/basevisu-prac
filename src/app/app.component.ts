819db033c9d00c41c23f422282e6f362266c5407
mdmostafa15 <mostafaka321@gmail.com>
2025-02-16 20:02:15 +0600

    user interactions are added on Header Bar components : basevisu-prac

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import {Ui5WebcomponentsModule} from '@ui5/webcomponents-ngx'
+import {MenuItemComponent, Ui5WebcomponentsModule} from '@ui5/webcomponents-ngx'
 import { SideMenuComponent } from "./side-menu/side-menu.component";
 import { TableContainerComponent } from "./table-container/table-container.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Ui5WebcomponentsModule, SideMenuComponent, TableContainerComponent],
+  
+  imports: [RouterOutlet, Ui5WebcomponentsModule, SideMenuComponent, TableContainerComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basevisu-prac';
+  lang: string="BN";
+  filterItem: string='filter item 1';
+  flag: boolean = false;
+  showItemList() {
+    this.flag = !this.flag;
+  }
+  setLang(lang: string ) {
+    console.log("set language: ");
+    this.lang = lang;
+  }
+  logOut() {
+    console.log("logOut clicked!!!");
+    
+  }
 }
